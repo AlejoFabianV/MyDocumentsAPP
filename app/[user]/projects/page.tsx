6d2de09b82cc6d4db0b1a16f994c02b6202a155d
AlejoFabianV <verhagenalejo@gmail.com>
2024-12-23 19:11:22 -0300

    proyectos del usuario traidos de la DB

--- app/[user]/projects/page.tsx
+++ app/[user]/projects/page.tsx
@@ -1,7 +1,31 @@
-export default function Project() {
+//server component
+import { createClient } from "@/utils/supabase/server";
+
+
+export default async function Project() {
+    //instancia de renderizado del componente
+    const supabase = await createClient();
+    const { data, error} = await supabase.from('project').select(`
+        id,
+        name_project,
+        last_update,
+        user(name)
+    `)
+
+    console.log(data, error)
+
     return (
         <>
-            <h1>Proyectos</h1>
+            <ul>
+                esto funciona supuestamente
+            </ul> 
+        {data?.map((project: any) => (
+            <li key={project.id}>
+                <h3>{project.name_project}</h3>
+                <p>Last Update: {project.last_update}</p>
+                <p>Author: {project.user.name}</p>
+            </li>
+        ))}
         </>
     );
 }
